7e367a5b095811bf571a2d234ed1c4ad9ee8c8b0
TongguangZhang <tonggz250@gmail.com>
2023-09-09 15:03:00 -0400

    recording scrums in history

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Tong Zhang on 2023-09-09.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    let attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
