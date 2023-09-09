46992b536f88b1ebfe6cfa3a33992dd7f8fab7c0
TongguangZhang <tonggz250@gmail.com>
2023-09-09 14:12:53 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tong Zhang on 2023-09-09.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
